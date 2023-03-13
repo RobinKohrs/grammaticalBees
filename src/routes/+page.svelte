a5e1b3ab1bfbaf829ac9d583a4f6842253af57b4
RobinKohrs <robin.kohrs@gmx.de>
2023-03-13 21:38:28 +0100

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  import text from "./../assets/text.json";
+</script>
+
+<h1>The text</h1>
+<div class="container">
+  <span>
+    {JSON.stringify(text)}
+  </span>
+</div>
+
+<style lang="scss">
+  .container {
+    max-width: 400px;
+    margin: 0 auto;
+    & pre {
+      color: darkblue;
+    }
+  }
+</style>
